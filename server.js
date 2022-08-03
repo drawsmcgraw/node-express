eed1d9083a5554aa1236b995d2241d39d2498a3a
Drew Malone <drew.malone@gmail.com>
2022-08-03 10:05:55 -0400

    update message

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from the server!');
+  res.send('Hello from the server! Now with easy updates!');
 });
 
 app.get('/health', (req, res) => {
